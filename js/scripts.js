38aa93cecb05c8fa2807701530c903c6847dfa81
Jenna Montero and Tam Nguyen <3vivalagitana3@gmail.com>
2016-06-22 20:19:36 -0700

    pass consonant followed by qu spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ var pig_latin = function(x) {
 
   if (x[0] === "a" || x[0] === "e" || x[0] === "i" || x[0] === "o" || x[0] === "u" ) {
     return x + "ay";
+  } else if (x[1] === "q") {
+    return x.slice(3) + x[0] + x[1] + x[2] + "ay";
   } else if (x[0] === "q") {
     return x.slice(2) + x[0] + x[1] + "ay";
   } else if ( x[0, 1, 2] !== "a" || x[0, 1, 2] !== "e" || x[0, 1, 2] !== "i" || x[0, 1, 2] !== "o" || x[0, 1, 2] !== "u" ) {
